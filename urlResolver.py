4ac1fa11f1876b581a292470e2b8eeb8de9be7a3
paolomorettin <morettin.paolo@gmail.com>
2015-05-13 16:25:02 +0100

    url resolver

---
+++ urlResolver.py
@@ -0,0 +1,116 @@
+import json
+import requests
+from threading import Thread
+from Queue import Queue
+
+def resolve(shortener,max_redirects=5,timeout=5) :
+    session = requests.session()
+    r = session.get(shortener, allow_redirects=False, timeout=timeout)
+    resolved = r.url
+    red_count = 0
+    for redirect in session.resolve_redirects(r, r.request, timeout=timeout):
+        red_count += 1
+        resolved = redirect.url
+        if red_count >= max_redirects :
+            break
+    return resolved
+
+
+if __name__ == '__main__' :
+    
+    from sys import argv
+    
+    output_path = argv[1]
+
+    previously_resolved = set()
+    try :
+        with open(output_path,'r') as f :
+            for line in f :
+                try :
+                    k,_ = line.strip().split(',')
+                    previously_resolved.add(k)
+                except ValueError :
+                    continue
+    except IOError :
+        pass
+    
+    urls = set()    
+    t_path = lambda x : './data/tweets_{}.json'.format(str(x))
+    for x in xrange(10) :
+        try :
+            with open(t_path(x),'r') as f :
+                print "Reading",t_path(x)
+                for line in f :
+                    try :
+                        t = json.loads(line)
+                    except ValueError :
+                        continue
+                    if 'entities' in t and 'urls' in t['entities'] :
+                        for u in t['entities']['urls'] :
+                            exurl = u['expanded_url']
+                            if not exurl in previously_resolved :
+                                urls.add( exurl )
+        except IOError :
+            continue
+
+
+    # filter with shortener domain list            
+    with open('shorturls.txt','r') as f :
+        s_domains = set()
+        for line in f :
+            if line != '' :
+                s_domains.add( line.strip() )
+
+    with open(output_path,'a') as f :
+
+        n_threads = 100
+        in_queue = Queue()
+        out_queue = Queue()
+
+
+        for url in urls :
+            domain = url.partition('//')[-1].partition('/')[0]
+            if domain in s_domains :
+                in_queue.put( url )
+
+            else :
+                try :
+                    f.write('{},{}\n'.format(url,url))
+                except Exception :
+                    continue
+
+        def worker(in_queue,out_queue):
+            while True :
+                url = in_queue.get()
+                try :
+                    rurl = resolve(url)
+                    out_queue.put((url,rurl))
+                except Exception :
+                    pass
+                in_queue.task_done()
+
+
+
+        for i in range(n_threads):
+            t = Thread(target=worker,args=(in_queue,out_queue))
+            t.daemon = True
+            t.start()
+        
+        try:
+            print "Waiting for the workers to finish"
+            in_queue.join()
+            print "Workers finished crunching"
+        except KeyboardInterrupt :
+            pass
+
+        while not out_queue.empty() :
+            k,v = out_queue.get()
+            try :
+                f.write('{},{}\n'.format(k,v))
+            except Exception :
+                continue
+        
+
+            
+
+    
