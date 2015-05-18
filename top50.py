2ab0990d1938212bdd1a9ad42412572496be89c7
paolomorettin <morettin.paolo@gmail.com>
2015-05-18 11:19:00 +0100

    top50

---
+++ top50.py
@@ -0,0 +1,24 @@
+
+from sys import argv
+
+path = argv[1]
+
+domains = {}
+with open(path,'r') as f :
+    for line in f :
+        try :
+            _,url = line.strip().split(',')
+        except ValueError :
+            continue
+
+        if 'www.' in url :
+            url = url.replace('www.','')
+        dom = url.partition('.')[0]
+        if dom in domains :
+            domains[dom] += 1
+        else :
+            domains[dom] = 1
+
+ranked = sorted(domains.items(),key = lambda x : x[1], reverse = True)
+for d,c, in ranked[:100] :
+    print '{},{}'.format(str(d),str(c))
